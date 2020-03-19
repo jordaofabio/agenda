8d9e6c465255f494b203996a19e9c8f72e8de8f8
Fabio <fabiosjordao@gmail.com>
2020-03-18 21:05:54 -0300

    Notificações com mongo

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,29 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 2,
+      offset: (page - 1) * 2,
+      include: [{ model: User, as: 'provider' }],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -58,25 +78,18 @@ class AppointmentController {
       date,
     });
 
-    return res.json(appointment);
-  }
+    const user = await User.findByPk(req.userId);
 
-  async index(req, res) {
-    const { page = 1 } = req.query;
-
-    const appointments = await Appointment.findAll({
-      where: {
-        user_id: req.userId,
-        canceled_at: null,
-      },
-      order: ['date'],
-      attributes: ['id', 'date'],
-      limit: 2,
-      offset: (page - 1) * 2,
-      include: [{ model: User, as: 'provider' }],
+    const formatedDate = format(hourStart, "'dia' dd 'de' MMMM', às 'H:mm'h'", {
+      locale: ptBr,
     });
 
-    return res.json(appointments);
+    await Notification.create({
+      content: `Novo agendamento: ${user.name} agendou um horário ${formatedDate} `,
+      user: provider_id,
+    });
+
+    return res.json(appointment);
   }
 }
 
