3ca5fc6d287516d51945f558305eac86272693cd
Fabio <fabiosjordao@gmail.com>
2020-03-21 00:19:05 -0300

    Criando e notificação e marcando como lida

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import SheduleController from './app/controllers/SheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(mulderConfig);
@@ -24,5 +25,8 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 
 routes.get('/shedule', SheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
