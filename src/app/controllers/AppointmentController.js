da95e99ac7e48b7dc3c3d12320164f11a4970f4a
Fabio <fabiosjordao@gmail.com>
2020-03-23 01:13:14 -0300

    Envio de email com template

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,7 @@ import ptBr from 'date-fns/locale/pt-BR';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -99,7 +100,20 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -119,6 +133,19 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às 'H:mm'h'", {
+          locale: ptBr,
+        }),
+      },
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'db282ce6acac3c',
+    pass: '2fd2a1139aeac8',
+  },
+  default: {
+    from: 'Equipe Angenda Dev <equipe@agendadev.com>',
+  },
+};
